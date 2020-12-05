47ac09309925062a229ae0f2d6166c2ab9bff4c9
Maxim Claeys <maxim.claeys@robovision.eu>
2020-12-05 09:10:59 +0100

    Day 5

--- aoc2020/solvers/implementations/__init__.py
+++ aoc2020/solvers/implementations/__init__.py
@@ -2,10 +2,12 @@ from .day1 import SolverDay1
 from .day2 import SolverDay2
 from .day3 import SolverDay3
 from .day4 import SolverDay4
+from .day5 import SolverDay5
 
 __all__ = [
     "SolverDay1",
     "SolverDay2",
     "SolverDay3",
     "SolverDay4",
+    "SolverDay5",
 ]
---
+++ aoc2020/solvers/implementations/day5.py
@@ -0,0 +1,53 @@
+import logging
+import re
+from pathlib import Path
+from typing import List
+
+from aoc2020.solvers import PuzzleSolver, SolverFactory
+
+logger = logging.getLogger("SolverDay5")
+
+
+@SolverFactory.register(day=5)
+class SolverDay5(PuzzleSolver):
+    def __init__(self, input_file: Path):
+        super().__init__(input_file=input_file)
+
+    @property
+    def demo_result_1(self) -> int:
+        return 820
+
+    @property
+    def demo_result_2(self) -> int:
+        logger.warning(f"Demo result 2 for day 5 not available yet")
+        return 0
+
+    def _read_file(self) -> List[int]:
+        seat_ids: List[int] = []
+        pattern = "^[F|B]{7}[L|R]{3}$"
+        for line in self._input_file.open(mode="r"):
+            line = line.strip()
+            if re.fullmatch(pattern, line) is not None:
+                binary = line.replace("F", "0").replace("B", "1").replace("L", "0").replace("R", "1")
+                seat_id = int(binary, 2)
+                logger.debug(f"Decoded seat ID is {seat_id}")
+                seat_ids.append(seat_id)
+            else:
+                logger.error(f"Invalid pass detected: {line}")
+        return seat_ids
+
+    def solve_1(self) -> int:
+        return max(self._input_data)
+
+    def solve_2(self) -> int:
+        max_id = max(self._input_data)
+        # Get all seat numbers
+        all_seats = set(range(max_id+1))
+        # Get missing seat numbers
+        missing_seats = all_seats - set(self._input_data)
+        # Find missing seat for which adjacent seats are not missing
+        for seat in missing_seats:
+            if seat-1 not in missing_seats and seat+1 not in missing_seats:
+                return seat
+        logger.error(f"No solution found")
+        return 0
