d1951371903d13fd98b15c9cd6625a15cdca09df
Maxim Claeys <maxim.claeys@robovision.eu>
2020-12-22 11:29:47 +0100

    Add looped approach to avoid recursion overhead

--- aoc2020/solvers/implementations/day22.py
+++ aoc2020/solvers/implementations/day22.py
@@ -1,5 +1,4 @@
 import logging
-import sys
 from collections import defaultdict, deque
 from pathlib import Path
 from typing import Deque, Dict, List, Optional, Set, Tuple
@@ -7,8 +6,6 @@ from typing import Deque, Dict, List, Optional, Set, Tuple
 from aoc2020.solvers import PuzzleSolver, SolverFactory
 
 logger = logging.getLogger("SolverDay22")
-# Increase recursion limit
-sys.setrecursionlimit(10**6)
 
 
 @SolverFactory.register(day=22)
@@ -54,12 +51,10 @@ class SolverDay22(PuzzleSolver):
             return 2, deck1
         card1 = deck1.popleft()
         card2 = deck2.popleft()
-        if card1 > card2:
-            deck1.append(card1)
-            deck1.append(card2)
-        else:
-            deck2.append(card2)
-            deck2.append(card1)
+        winner = 1 if card1 > card2 else 2
+        SolverDay22._update_decks(
+            card1=card1, card2=card2, deck1=deck1, deck2=deck2, winner=winner
+        )
         return SolverDay22._play_normal(deck1, deck2)
 
     @staticmethod
@@ -92,13 +87,73 @@ class SolverDay22(PuzzleSolver):
         # When someone has not enough cards, player with highest card wins
         else:
             winner = 1 if card1 > card2 else 2
+
         # Add cards to winners deck and recurse
-        winner_deck = deck1 if winner == 1 else deck2
-        winner_deck.append(card1 if winner == 1 else card2)
-        winner_deck.append(card2 if winner == 1 else card1)
+        SolverDay22._update_decks(
+            card1=card1, card2=card2, deck1=deck1, deck2=deck2, winner=winner
+        )
         return SolverDay22._play_recursive(
             deck1=deck1, deck2=deck2, history=history,
         )
+
+    @staticmethod
+    def _play_looped(
+        deck1: Deque[int],
+        deck2: Deque[int],
+    ) -> Tuple[int, Deque[int]]:
+        history: Set[int] = set()
+        stack: Deque[Tuple[int, int, Deque[int], Deque[int], Set[int]]] = deque()
+        while True:
+            # Key for state: hash of two decks
+            key = hash((tuple(deck1), tuple(deck2)))
+            # Player 1 wins if in state seen before or when player 2 has no cards
+            winner = 1 if len(deck2) == 0 or key in history else 0
+            # Player 2 wins if player 1 has no cards
+            winner = 2 if len(deck1) == 0 else winner
+            if winner > 0:
+                # When we still have stacked results to process, do so
+                if len(stack) > 0:
+                    card1, card2, deck1, deck2, history = stack.pop()
+                    SolverDay22._update_decks(
+                        card1=card1, card2=card2,
+                        deck1=deck1, deck2=deck2,
+                        winner=winner
+                    )
+                    continue
+                else:
+                    return winner, deck1 if winner == 1 else deck2
+            # No winner, play round
+            history.add(key)
+            card1 = deck1.popleft()
+            card2 = deck2.popleft()
+            # When both players have enough cards for recursion, recurse
+            if card1 <= len(deck1) and card2 <= len(deck2):
+                # Append current state to the stack
+                stack.append((card1, card2, deck1, deck2, history))
+                # Update parameters and loop
+                deck1 = deque(list(deck1)[:card1])
+                deck2 = deque(list(deck2)[:card2])
+                history = set()
+                continue
+            # When someone has not enough cards, player with highest card wins
+            else:
+                winner = 1 if card1 > card2 else 2
+                # Add cards to winners deck and loop
+                SolverDay22._update_decks(
+                    card1=card1, card2=card2,
+                    deck1=deck1, deck2=deck2,
+                    winner=winner
+                )
+
+    @staticmethod
+    def _update_decks(
+        card1: int, card2: int,
+        deck1: Deque[int], deck2: Deque[int],
+        winner: int,
+    ) -> None:
+        winner_deck = deck1 if winner == 1 else deck2
+        winner_deck.append(card1 if winner == 1 else card2)
+        winner_deck.append(card2 if winner == 1 else card1)
 
     @staticmethod
     def _score_deck(deck: Deque[int]) -> int:
@@ -118,5 +173,12 @@ class SolverDay22(PuzzleSolver):
         decks = self._input_data
         deck1 = deque(decks[1])
         deck2 = deque(decks[2])
-        _, winner_deck = self._play_recursive(deck1, deck2, set())
+        _, winner_deck = self._play_looped(deck1, deck2)
+        # When we want to use the recursive approach (cleaner IMO), we have
+        #  to increase the recursion limit. Moving to tail recursion (similar
+        #  to what happens in the looped approach) does not help because
+        #  Python doesn't have tail recursion optimization.
+        # import sys
+        # sys.setrecursionlimit(10**6)
+        # _, winner_deck = self._play_recursive(deck1, deck2)
         return self._score_deck(winner_deck)
